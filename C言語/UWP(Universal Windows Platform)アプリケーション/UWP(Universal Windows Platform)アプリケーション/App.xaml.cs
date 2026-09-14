b5264cf3146b8adfc97079222eab500db90cea97
naruto1231 <mochitokiaaa@gmail.com>
2026-09-15 07:02:20 +0900

    C言語プログラムを追加

---
+++ C言語/UWP(Universal Windows Platform)アプリケーション/UWP(Universal Windows Platform)アプリケーション/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UWP_Universal_Windows_Platform_アプリケーション
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
